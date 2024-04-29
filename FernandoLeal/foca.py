86e4d6ce90d1e9ac60b78ad0ad30888287cfd0dd
DracheLoyalty <yolender69@hotmail.com>
2024-04-29 02:43:24 -0600

    Se agregan propiedades y sus metodos

--- FernandoLeal/foca.py
+++ FernandoLeal/foca.py
@@ -12,3 +12,32 @@ class foca:
     @nombre.setter
     def nombre(self,valor):
         self.__nombre=valor
+
+    @property
+    def peso(self):
+        return self.__peso
+    @peso.setter
+    def peso(self,valor):
+        self.__peso=valor
+
+    @property
+    def edad(self):
+        return self.__edad
+    @edad.setter
+    def edad(self,valor):
+        self.__edad=valor
+
+    @property
+    def sexo(self):
+        return self.__sexo
+    @sexo.setter
+    def sexo(self,valor):
+        self.__sexo=valor
+
+    @property
+    def color(self):
+        return self.__color
+    @color.setter
+    def color(self,valor):
+        self.__color=valor
+    
