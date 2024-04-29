99c1323fe3f35acbbde947d39d41f59895a59142
DracheLoyalty <yolender69@hotmail.com>
2024-04-29 03:57:16 -0600

    Defini metodos pero aun me falta por completar

--- FernandoLeal/foca.py
+++ FernandoLeal/foca.py
@@ -41,3 +41,18 @@ class foca:
     def color(self,valor):
         self.__color=valor
     
+    def alimentacion(self):
+        print("{self.__nombre},generalmente su dieta se basa en peces, calamares y crustáceos.")
+
+    def habitat(self):
+        print("Este pequeño {self.__nombre}, le encanta las aguas frias del oceano, de preferencia cerca de las costas.")
+
+    def categoria(self):
+        print("Aun asi sabias que {self.__nombre}, pertenece a los grandes mamiferos marinos")
+
+    def nombreCientifico(self):
+        print("Talvez tu lo conoces como {self.__nombre} o foca, pero en su origen se le conoce como  Phoca vitulina")
+
+    def aspecto(self):
+        print("{self.__nombre}, hay como muchos y pueden llegar a tener cuerpos alargados y adaptados para nadar en el agua, ademas su color puede llegar a ser gris oscuro hasta marron claro")
+    pass
