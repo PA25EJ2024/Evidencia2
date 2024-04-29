1196643e8abf4b29c17a1f2ee04616f2a7c62e03
DracheLoyalty <yolender69@hotmail.com>
2024-04-29 02:27:22 -0600

    Agregue instrucciones al archivo

--- FernandoLeal/foca.py
+++ FernandoLeal/foca.py
@@ -1,1 +1,14 @@
-#class foca:
+class foca:
+    def __init__(self, nombre, peso, edad, sexo, color):
+        self.nombre = nombre
+        self.peso = peso
+        self.edad = edad
+        self.sexo = sexo
+        self.color = color
+
+    @property
+    def nombre(self):
+        return self.__nombre
+    @nombre.setter
+    def nombre(self,valor):
+        self.__nombre=valor
